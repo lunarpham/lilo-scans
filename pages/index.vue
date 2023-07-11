8c0bfab088b0069e48ded6cc0ff7a708c896c55d
TeeUsagii <tiennguyen2003.b@gmail.com>
2023-07-11 14:47:46 +0700

    up to date

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-<<<<<<< HEAD
       <header>
           <div class="container-fluid" id="masthead">
           </div>
@@ -11,7 +10,6 @@
               <ul class="new-updates p-0">
                   <li class="update-content my-3">
                       <a href="#" class="container bg-white shadow-sm p-0 d-flex link-underline link-underline-opacity-0 link-secondary">
-                          <div class="start-block"></div>
                           <div class="update-body w-100 py-2 px-lg-5 row">
                               <div class="col-md-8">Anh Tiến Phiêu Lưu Ký</div>
                               <div class="col-md-1">Chương 5</div>
@@ -21,7 +19,6 @@
                   </li>
                   <li class="update-content my-3">
                       <a href="#" class="container bg-white shadow-sm p-0 d-flex link-underline link-underline-opacity-0 link-secondary">
-                          <div class="start-block"></div>
                           <div class="update-body w-100 py-2 px-lg-5 row">
                               <div class="col-md-8">Anh Tiến Phiêu Lưu Ký</div>
                               <div class="col-md-1">Chương 4</div>
@@ -31,7 +28,6 @@
                   </li>
                   <li class="update-content my-3">
                       <a href="#" class="container bg-white shadow-sm p-0 d-flex link-underline link-underline-opacity-0 link-secondary">
-                          <div class="start-block"></div>
                           <div class="update-body w-100 py-2 px-lg-5 row">
                               <div class="col-md-8">Anh Tiến Phiêu Lưu Ký</div>
                               <div class="col-md-1">Chương 3</div>
@@ -40,8 +36,7 @@
                       </a>
                   </li>
                   <li class="update-content my-3">
-                      <a href="#" class="container bg-white shadow-sm p-0 d-flex link-underline link-underline-opacity-0 link-secondary">
-                          <div class="start-block"></div>
+                      <a href="#" class="rounded container bg-white shadow-sm p-0 d-flex link-underline link-underline-opacity-0 link-secondary">
                           <div class="update-body w-100 py-2 px-lg-5 row">
                               <div class="col-md-8">Địt bà Nt*da và BLT</div>
                               <div class="col-md-1">Chương 1</div>
@@ -54,67 +49,11 @@
       </section>
       <section id="all-mangas" class="pt-5">
           <div class="container">
-=======
-        <header>
-            <div class="container-fluid" id="masthead">
-            </div>
-        </header>
-        <section id="updates" class="pt-5">
-            <div class="container">
-                <h5 class="fw-bold">MỚI CẬP NHẬT</h5>
-                <ul class="new-updates p-0">
-                    <li class="update-content my-3">
-                        <a href="#"
-                            class="border-left rounded container bg-white shadow-sm p-0 d-flex link-underline link-underline-opacity-0 link-secondary">
-                            <div class="update-body w-100 py-2 px-lg-5 row">
-                                <div class="col-md-8">Anh Tiến Phiêu Lưu Ký</div>
-                                <div class="col-md-1">Chương 5</div>
-                                <div class="col-md-3 text-end">16 giờ trước</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="update-content my-3">
-                        <a href="#"
-                            class="border-left rounded container bg-white shadow-sm p-0 d-flex link-underline link-underline-opacity-0 link-secondary">
-                            <div class="update-body w-100 py-2 px-lg-5 row">
-                                <div class="col-md-8">Anh Tiến Phiêu Lưu Ký</div>
-                                <div class="col-md-1">Chương 4</div>
-                                <div class="col-md-3 text-end">2 ngày trước</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="update-content my-3">
-                        <a href="#"
-                            class="border-left rounded container bg-white shadow-sm p-0 d-flex link-underline link-underline-opacity-0 link-secondary">
-                            <div class="update-body w-100 py-2 px-lg-5 row">
-                                <div class="col-md-8">Anh Tiến Phiêu Lưu Ký</div>
-                                <div class="col-md-1">Chương 3</div>
-                                <div class="col-md-3 text-end">3 ngày trước</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="update-content my-3">
-                        <a href="#"
-                            class="border-left rounded container bg-white shadow-sm p-0 d-flex link-underline link-underline-opacity-0 link-secondary">
-                            <div class="update-body w-100 py-2 px-lg-5 row">
-                                <div class="col-md-8">Địt bà Nt*da và BLT</div>
-                                <div class="col-md-1">Chương 1</div>
-                                <div class="col-md-3 text-end">3 ngày trước</div>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </section>
-        <section id="all-mangas" class="pt-5">
-            <div class="container">
->>>>>>> abb74de06f4b0a8093551c94bee50ed697b6e30f
                 <h5 class="fw-bold">TẤT CẢ MANGA</h5>
                 <ul v-for="manga in mangas">
                     <img :src="manga.img" :alt="manga.id" style="width: 10rem;">
                     <NuxtLink :to="`manga/${manga.id}`">{{ manga.title }}</NuxtLink>
                 </ul>
-<<<<<<< HEAD
           </div>
       </section>
     </div>
@@ -150,43 +89,6 @@
     })
 
     const { data: mangas } = await useFetch('https://api.npoint.io/5db90e7f047a1396a59c/manga');
-=======
-            </div>
-        </section>
-    </div>
-</template>
-<style>
-@import 'bootstrap/dist/css/bootstrap.css';
-@import '~/css/styles.css'
-</style>
-<script setup>
-useHead({
-    title: 'Trang chủ - Lilo Translation',
-    meta: [
-        {
-            hid: 'description',
-            name: 'description',
-            content: 'Lilo Translation'
-        }
-    ],
-    link: [
-        {
-            rel: 'icon',
-            type: 'image/x-icon',
-            href: '/lilo.webp'
-        }
-    ],
-
-    script: [
-        {
-            src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js',
-            ssr: true
-        }
-    ]
-})
-
-const { data: mangas } = await useFetch('https://api.npoint.io/5db90e7f047a1396a59c/manga');
->>>>>>> abb74de06f4b0a8093551c94bee50ed697b6e30f
 
 </script>
   
