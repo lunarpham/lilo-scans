efa9af9df959b6e578e8497b728ff42508f277f0
TeeUsagii <tiennguyen2003.b@gmail.com>
2023-07-11 17:17:53 +0700

    animation nav

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,8 +5,8 @@
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                    <li class="nav-item fw-bold"><NuxtLink to="/" class="nav-link"><div class="active full-width">Trang chủ</div></NuxtLink></li>
-                    <li class="nav-item fw-bold"><NuxtLink to="/about" class="nav-link">Biết bố mày là ai không?</NuxtLink></li>
+                    <li class="nav-item fw-bold"><NuxtLink to="/" class="nav-link"><div class="line-bottom full-width">Trang chủ</div></NuxtLink></li>
+                    <li class="nav-item fw-bold"><NuxtLink to="/about" class="nav-link"><div class="line-bottom full-width">Biết bố mày là ai không?</div></NuxtLink></li>
                 </ul>
             </div>
         </div>
--- pages/about.vue
+++ pages/about.vue
@@ -3,34 +3,34 @@
     </div>
 </template>
 <style>
-    @import 'bootstrap/dist/css/bootstrap.css';
-    @import '~/css/styles.css'
+@import 'bootstrap/dist/css/bootstrap.css';
+@import '~/css/styles.css'
 </style>
 <script setup>
-    useHead({
-        title: 'Thông tin - Lilo Translation',
-        meta: [
-            {
+useHead({
+    title: 'Thông tin - Lilo Translation',
+    meta: [
+        {
             hid: 'description',
             name: 'description',
             content: 'Lilo Translation'
-            }
-        ],
-        link: [
-            {
+        }
+    ],
+    link: [
+        {
             rel: 'icon',
             type: 'image/x-icon',
             href: '/lilo.webp'
-            }
-        ],
+        }
+    ],
 
-        script: [
-            {
+    script: [
+        {
             src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js',
             ssr: true
-            }
-        ]
-    })
+        }
+    ]
+})
 </script>
   
   
