b597c65d23990a60feb5d7ee0fcb62680223a62a
TeeUsagii <tiennguyen2003.b@gmail.com>
2023-07-14 16:50:21 +0700

    fix about page and add code follow

--- pages/about.vue
+++ pages/about.vue
@@ -2,46 +2,81 @@
     <div>
         <header></header>
 
-        <body>
-            <div class="row container">
-                <div class="row col-xl-12 info-about">
-                    <div class="col-3">
-                        <div>hi</div>
+        <selection>
+            <div class="container">
+                <div class="row px-4">
+                    <div class="row col-xl-12 info-about">
+                        <div class="col-12 col-md-3">
+                            <div class="pb-2">
+                                <img src="https://i.imgur.com/PU44ah4.jpg" alt="Lilo Translation"
+                                    class="rounded-circle border border-danger-subtle" style="width:100%;">
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-9">
+                            <div class="row">
+                                <h5 class="col-12 fs-2 fw-semibold">Lilo Translation</h5>
+                                <h3 class="col-12 fw-medium">@liloscans</h3>
+                                <p class="col-12 mt-2">Nhóm dịch manga cấp thấp. Chủ yếu làm mấy bộ ít ai để ý tới.</p>
+                            </div>
+                            <div class="row mt-1">
+                                <div class="col-12 col-md-4 mt-2">
+                                    <div class="col-12">Ngôn ngữ</div>
+                                    <div class="col-12 fw-semibold">Nhật - Anh - Việt</div>
+
+                                </div>
+                                <div class="col-12 col-md-4 mt-2">
+                                    <div class="col-12">Ngày lập</div>
+                                    <div class="col-12 fw-semibold">13-07-2022</div>
+                                </div>
+                                <div class="col-12 col-md-4 mt-2">
+                                    <div class="col-12">Thành viên</div>
+                                    <div class="col-12 fw-semibold">10</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-9">
+                    <div class="col-xl-12 follow">
                         <div class="row">
-                            <div class="col-12 fs-2 fw-semibold">Lilo Translation</div>
-                            <div class="col-12 fw-medium">@liloscans</div>
-                            <div class="col-12 mt-2">Nhóm dịch manga cấp thấp. Chủ yếu làm mấy bộ ít ai để ý tới.</div>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-12 col-md-4 mt-2">
-                                <div class="col-12">Ngôn ngữ</div>    
-                                <div class="col-12 fw-semibold">Nhật - Anh - Việt</div>
-                                
+                            <div class="col-md-8 mt-5">
+                                <div>
+                                    <h5 class="fw-bold no-wrap-content border-bottom-grey" >THEO DÕI BỌN MÌNH TẠI</h5>
+
+                                </div>
+                                <div class="d-flex flex-wrap flex-row mt-3">
+                                    <a href="/" rel="noopener noreferrer" target="_blank" class="custom-box rounded-3">
+                                        Facebook </a>
+                                    <a href="/" rel="noopener noreferrer" target="_blank" class="custom-box rounded-3">
+                                        Mangadex
+                                    </a>
+                                    <a href="/" rel="noopener noreferrer" target="_blank" class="custom-box rounded-3">
+                                        Otakusan </a>
+                                    <a href="/" rel="noopener noreferrer" target="_blank" class="custom-box rounded-3">
+                                        Cuutruyen </a>
+                                </div>
                             </div>
-                            <div class="col-12 col-md-4 mt-2">
-                                <div class="col-12">Ngày lập</div>    
-                                <div class="col-12 fw-semibold">13-07-2022</div>
-                            </div>
-                            <div class="col-12 col-md-4 mt-2">
-                                <div class="col-12">Thành viên</div>    
-                                <div class="col-12 fw-semibold">10</div> 
+                            <div class="col-md-4 mt-5">
+                                <div>
+                                    <h5 class="fw-bold no-wrap-content border-bottom-grey">LIÊN HỆ</h5>
+
+                                </div>
+                                <div class="mt-3"><a href="mailto:liloscans@gmail.com" rel="noopener noreferrer"
+                                        target="_blank"
+                                        class="custom-box rounded-3 d-flex flex-wrap flex-row justify-content-center w-100"><i
+                                            class="material-icons">mail</i>
+                                        <p class="mb-0 ms-2">EMAIL</p>
+                                    </a></div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-xl-12 follow">
+                    <div class="col-xl-12 member">
 
-                </div>
-                <div class="col-xl-12 member">
+                    </div>
+                    <div class="col-xl-12 collab">
 
-                </div>
-                <div class="col-xl-12 collab">
-
+                    </div>
                 </div>
             </div>
-        </body>
+        </selection>
 
         <footer></footer>
     </div>
@@ -52,7 +87,5 @@ useHead({
 })
 </script>
 
-<style>
-@import 'bootstrap/dist/css/bootstrap.css';
-@import '~/css/styles.css';
-</style>
+<style>@import 'bootstrap/dist/css/bootstrap.css';
+@import '~/css/styles.css';</style>
