b4a6733a7b3aa55a0d06119cb3fc306848532549
TeeUsagii <tiennguyen2003.b@gmail.com>
2023-07-14 15:26:58 +0700

    about page v1

--- pages/about.vue
+++ pages/about.vue
@@ -1,26 +1,58 @@
 <template>
     <div>
-        <section class="mt-5 pt-5">
-            <div class="container px-4">
-                <h1>Thông tin</h1>
-                <p>Đây là trang thông tin</p>   
+        <header></header>
+
+        <body>
+            <div class="row container">
+                <div class="row col-xl-12 info-about">
+                    <div class="col-3">
+                        <div>hi</div>
+                    </div>
+                    <div class="col-9">
+                        <div class="row">
+                            <div class="col-12 fs-2 fw-semibold">Lilo Translation</div>
+                            <div class="col-12 fw-medium">@liloscans</div>
+                            <div class="col-12 mt-2">Nhóm dịch manga cấp thấp. Chủ yếu làm mấy bộ ít ai để ý tới.</div>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col-12 col-md-4 mt-2">
+                                <div class="col-12">Ngôn ngữ</div>    
+                                <div class="col-12 fw-semibold">Nhật - Anh - Việt</div>
+                                
+                            </div>
+                            <div class="col-12 col-md-4 mt-2">
+                                <div class="col-12">Ngày lập</div>    
+                                <div class="col-12 fw-semibold">13-07-2022</div>
+                            </div>
+                            <div class="col-12 col-md-4 mt-2">
+                                <div class="col-12">Thành viên</div>    
+                                <div class="col-12 fw-semibold">10</div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-12 follow">
+
+                </div>
+                <div class="col-xl-12 member">
+
+                </div>
+                <div class="col-xl-12 collab">
+
+                </div>
             </div>
-        </section>
-        
+        </body>
+
+        <footer></footer>
     </div>
 </template>
 <script setup>
-    useHead({
-        title: 'Thông tin - Lilo Translation'
-    })
-    useSeoMeta({
-    ogTitle: 'Thông tin - Lilo Translation',
-    ogDescription: 'Về team của chúng mình.',
-    ogImage: 'https://i.imgur.com/PU44ah4.jpg',
-    twitterTitle: 'Thông tin - Lilo Translation',
-    twitterDescription: 'Về team của chúng mình.',
-    twitterImage: 'https://i.imgur.com/PU44ah4.jpg'
+useHead({
+    title: 'Trang chủ - Lilo Translation'
 })
 </script>
-  
-  
+
+<style>
+@import 'bootstrap/dist/css/bootstrap.css';
+@import '~/css/styles.css';
+</style>
