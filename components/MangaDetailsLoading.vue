78db664064fcbca6f693d98148c9fb92c061922b
Lunar Phạm <truong.pham210214@vnuk.edu.vn>
2023-09-16 20:42:42 +0700

    Adjust placeholder blocks

--- components/MangaDetailsLoading.vue
+++ components/MangaDetailsLoading.vue
@@ -11,8 +11,8 @@
                         </div>
                         <div class="container p-0 mt-3">
                             
-                            <div class="d-flex flex-column">
-                                <h1 class="placeholder col-12"></h1>
+                            <div class="d-flex flex-column placeholder-glow">
+                                <h1 class="placeholder col-12 mb-3"></h1>
                             </div>
                         </div>        
                         
@@ -23,47 +23,48 @@
                         
                         <!--Manga title and alias-->
                         <div class="row">
-                            <div class="col-md-12 mb-3">
-                                <h1 class="placeholder col-3"></h1>
-                                <h5 class="placeholder col-3"></h5>
+                            <div class="col-md-12 mb-3 d-flex flex-column placeholder-glow">
+                                <h1 class="placeholder col-8"></h1>
+                                <h5 class="placeholder col-6"></h5>
                             </div>
                         </div>
 
                         <!--Manga info R1-->
                         <div class="row">
-                            <div class="col-md-5 d-flex flex-row align-items-start">
-                                <p class="placeholder col-3">Tác giả</p>
-                                <p class="placeholder col-3"></p>
+                            <div class="col-md-5 d-flex flex-row align-items-start placeholder-glow">
+                                <p class="placeholder col-6"></p>
                             </div>
                         </div>
 
                         <!--Manga info R2-->
                         <div class="row">
-                            <div class="col-md-12 d-flex flex-column align-items-start">
-                                <div class="placeholder col-8">
+                            <div class="col-md-12 d-flex flex-column align-items-start placeholder-glow">
+                                <div class="placeholder col-8 mb-4">
                                 </div>
                             </div>
                         </div>
 
                         <!--Manga info R3-->
                         <div class="row">
-                            <div class="col-md-4 d-flex flex-column align-items-start">
-                                <p class="placeholder col-3"></p>
+                            <div class="col-md-4 d-flex flex-column align-items-start placeholder-glow">
+                                <div class="placeholder col-8 mb-4"></div>
                             </div>
                         </div>
 
                         <!--Manga info R4-->
                         <div class="row">
-                            <div class="col-md-12">
-                                <p class="placeholder col-12"></p>
+                            <div class="col-md-12 placeholder-glow">
+                                <p class="placeholder col-12 mb-0"></p>
+                                <p class="placeholder col-12 mb-0"></p>
+                                <p class="placeholder col-12 mb-0"></p>
+                                <p class="placeholder col-12 mb-0"></p>
                             </div>
                         </div>
 
                         <!--Manga info R5-->
                         <div class="row mt-3">
-                            <div class="col-md-12 d-flex flex-row align-items-start">
-                                <p class="me-2">Nhóm dịch</p>
-                                <div class="placeholder col-6">
+                            <div class="col-md-12 d-flex flex-row align-items-start placeholder-glow">
+                                <div class="placeholder col-6 mb-2">
                                 </div>
                             </div>
                         </div>
@@ -73,8 +74,8 @@
                                         <h5 class="me-2"><i class="fa-solid fa-list-ul"></i></h5>
                                         <h5 class="fw-bold">DANH SÁCH CHƯƠNG</h5>
                                     </div>
-                                    <div class="row flex-row mt-2 mx-2 p-1">
-                                            <h1 class="placeholder col-12"></h1>
+                                    <div class="row flex-row mt-2 mx-2 p-1 placeholder-glow">
+                                            <h1 class="placeholder col-10"></h1>
                                     
                                     </div>
                                     
